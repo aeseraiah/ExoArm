8b54e46386f131d99d1759829f4da9d33e42a464
Nicole <ncm1@iu.edu>
2022-11-29 19:54:09 -0500

    add more for model training/data collected

--- modelTraining/trainModel.py
+++ modelTraining/trainModel.py
@@ -1,13 +1,22 @@
 from sklearn_rvm.em_rvm import EMRVC
 import pandas as pd
-from micromlgen import port_rvm
+# from micromlgen import port_rvm
 
-data = pd.read_csv()
+# data = pd.read_csv()
 
+# classifiers
+flexion = 0
+extension = 1
+
+feature1 = [[0,0,300,300, 0,0],[0,0,300,400, 0,0],[0,0,500,500, 0,0],[0,0,500,600, 0,0]]
+y1 = [0, 0, 1, 1]
 # trainingData = 
 # testingData = 
 
-model = EMRVC(kernel='rbf')
-model.fit(X, y)
+model = EMRVC(kernel='rbf', gamma="auto")
+model.fit(feature1, y1)
+
+X = [[0,0,350,320, 0,0]]
 prediction = model.predict(X)
+print(prediction)
 
