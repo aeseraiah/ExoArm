a5fe40d57a8c2a283b0a1eff357079dbef2c0e64
Nicole <ncm1@iu.edu>
2022-12-01 18:52:20 -0500

    add models

--- modelTraining/trainModelAvg.py
+++ modelTraining/trainModelAvg.py
@@ -22,23 +22,33 @@ window_size = 100
 # TURN DATA INTO NUMPY ARRAY
 np_data = data['2'].to_numpy()
 
-# plt.plot(np_data, color = 'blue')
-# for i in extension_startindexes:
-#     plt.plot(np_data[ i : i + window_size], color = 'green')
-# for i in flexion_startindexes:
-#     plt.plot(np_data[ i : i + window_size], color = 'red')
-# plt.title("FLEXION - RED , EXTENSION - GREEN")
-# plt.show()
+plt.plot(np_data, color = 'blue')
+for i in extension_startindexes:
+    plt.plot(data['2'][ i : i + window_size], color = 'green')
+for i in flexion_startindexes:
+    plt.plot(data['2'][ i : i + window_size], color = 'red')
+plt.title("FLEXION - RED , EXTENSION - GREEN")
+plt.show()
 
 
 # GENERATE FLEXION ARRAY AND EXTENSION ARRAY
-FLEXION_DATA = []
+FLEXION_DATA_ARRAY = []
 for i in flexion_startindexes:
-    FLEXION_DATA.append(np_data[ i : i + window_size])
+    FLEXION_DATA_ARRAY.append(np_data[ i : i + window_size])
+
+FLEXION_DATA = []
+for i in FLEXION_DATA_ARRAY:
+    FLEXION_DATA.append([np.mean(i)])
+# print(FLEXION_DATA)
+
+EXTENSION_DATA_ARRAY = []
+for i in extension_startindexes:
+    EXTENSION_DATA_ARRAY.append(np_data[ i : i + window_size])
 
 EXTENSION_DATA = []
-for i in extension_startindexes:
-    EXTENSION_DATA.append(np_data[ i : i + window_size])
+for i in EXTENSION_DATA_ARRAY:
+    EXTENSION_DATA.append([np.mean(i)])
+# print(EXTENSION_DATA)
 
 # classifiers
 flexion = 0
@@ -48,17 +58,16 @@ STOP_TRAIN = 6
 features = FLEXION_DATA[0:STOP_TRAIN] + EXTENSION_DATA[0:STOP_TRAIN]
 
 # y1 = [flexion, flexion, flexion, flexion, extension, extension, extension, extension]
-y1 = []
+y1=[]
 for i in range(0, STOP_TRAIN):
     y1.append(flexion)
 
 for i in range(0, STOP_TRAIN):
     y1.append(extension)
 
-model = EMRVC(kernel='linear', gamma="scale")
-model.fit(features, y1)
+model = EMRVC(kernel='rbf', gamma="scale", tol=1e10)
+model.fit(np.array(features).reshape(-1,1), y1)
 
-# X = [[0,0,350,320, 0,0]]
 X = FLEXION_DATA[STOP_TRAIN:] + EXTENSION_DATA[STOP_TRAIN:]
 prediction = model.predict(X)
 answ = [flexion, flexion, flexion, flexion, flexion, extension, extension, extension, extension, extension]
@@ -69,6 +78,6 @@ if prediction.tolist() != answ:
 else:
     print("PREDICTION:",prediction)
 
-# print("\nPREDICTION: ", prediction)
+print("\nPREDICTION: ", prediction)
 
 print("SCORE: ", model.score(X, answ))
